36a427471b83dc0a4cf05e927a5abe6e0fab5096
zakariabouchelaghem <z.bouchelaghem@esi-sba.dz>
2025-09-04 09:59:23 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+import numpy as np
+import cv2
+from io import BytesIO
+from PIL import Image
+import base64
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["https://zakariabouchelaghm.github.io"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = torch.load("vit_model.pth", map_location="cpu")  # or "cuda" if GPU available
+model.eval()
+
+with torch.no_grad():
+    input_tensor = torch.randn(1, 1, 32, 32)  # Example input tensor
+    outputs = model(input_tensor)   # input_tensor shape: [B, C, H, W]
+    predicted = outputs.argmax(dim=1)
+print("Predicted labels:", predicted)
+
+
+
+@app.post("/predict")
+
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(BytesIO(contents)).convert("L")
+
+    # Preprocessing (same as before)
+    img = np.array(image)
+    img = cv2.resize(img, (32, 32))
+    img = cv2.bitwise_not(img)
+    img = img / 255.0
+    img_input = np.array(img, dtype="float32")
+    img_input = np.expand_dims(img, axis=-1)   # (32, 32, 1)
+    img_input = np.expand_dims(img_input, axis=0)  # (1, 32, 32, 1)
+    img_input = torch.from_numpy(img_input).permute(0, 3, 1, 2)
+    img_input = (img_input - 0.5) / 0.5
+    with torch.no_grad():
+        outputs = model(img_input)   # input_tensor shape: [B, C, H, W]
+        predicted = outputs.argmax(dim=1)
+    return {"predicted_class": predicted.item()}
