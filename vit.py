3650b125e7564616c429a6dccc850fa76db1a948
Zakaria Bouchelaghem <58322682+zakariabouchelaghm@users.noreply.github.com>
2025-09-04 10:48:36 +0200

    Update vit.py

--- vit.py
+++ vit.py
@@ -1,17 +1,7 @@
-import numpy as np # linear algebra
-import pandas as pd
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader
-import torchvision
-from torchvision import datasets, transforms
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-
-
 
 
 class PatchEmbedding(nn.Module):
